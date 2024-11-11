00f45fd047abed9b8d9397731f921df08ff55160
D-Hite <Dylanhite@gmail.com>
2024-11-11 12:20:47 -0800

    update

--- collection/make_tables.py
+++ collection/make_tables.py
@@ -1,14 +1,21 @@
+
+# %%
+print('start')
+
+# %% 
 import pandas as pd
 import glob
 import time
 import duckdb
 
+# %%
 conn = duckdb.connect('firstdb.db')
+current_tables = ['advanced', 'fourfactors', 'misc', 'scoring', 'traditional']
 
+# %%
+conn.close()
 
-current_tables = ['advanced', 'fourfactors', 'misc', 'scoring']
-
-
+# %%
 ## log data
 log_csv_files = glob.glob(f'DATA/raw/log/*.csv')
 
@@ -23,9 +30,11 @@ for file in log_csv_files[1:]:
         INSERT INTO log_table
         SELECT * FROM read_csv_auto('{file}')
         """)
-
+# %%
 ## line data
-lines_csv_files = glob.glob(f'DATA/raw/lines/*.csv')
+lines_csv_files = glob.glob(f'DATA/lines/*.csv')
+# print(lines_csv_files)
+
 
 conn.execute(f"""
     CREATE OR REPLACE TABLE lines_table as
@@ -39,10 +48,11 @@ for file in lines_csv_files[1:]:
         SELECT * FROM read_csv_auto('{file}')
         """)
 
+# %%
 ## game data
 for tp in ['teams', 'players']:
     for kind in current_tables:
-        csv_files = glob.glob(f'DATA/raw/teams/{kind}/*.csv')
+        csv_files = glob.glob(f'DATA/raw/{tp}/{kind}/*.csv')
 
         conn.execute(f"""
             CREATE OR REPLACE TABLE {tp}_{kind} as
@@ -58,3 +68,309 @@ for tp in ['teams', 'players']:
                 """)
             
 ### todo: combine tables, processed data, dynamic query for model
+# %%
+conn.execute(f"""SHOW TABLES""").fetchall()
+
+# %%
+columns = conn.execute("SELECT table_name, column_name FROM information_schema.columns WHERE table_name ilike '%players%'").df()
+
+
+# %%
+print(columns)
+
+# %%
+
+# %%
+conn.execute(f"""
+    COPY 
+             (SELECT table_name, column_name 
+             FROM information_schema.columns 
+             WHERE table_name ilike '%players%'
+             )
+    to 'colstemp.csv' (HEADER, DELIMITER ',')
+             """).df()
+
+# %%
+conn.execute("""SELECT * 
+            FROM players_advanced 
+            limit 100""").df()
+
+
+
+
+
+# %%
+### COMBINE TEAM DATA
+conn.execute(f"""
+    CREATE OR REPLACE TABLE teams_combined2 as
+    SELECT *
+    FROM log_table lt
+    LEFT JOIN teams_advanced ta on lt.GAME_ID = ta.GAME_ID and lt.TEAM_ID = ta.TEAM_ID
+    LEFT JOIN teams_fourfactors tff on ta.GAME_ID = tff.GAME_ID and ta.TEAM_ID = tff.TEAM_ID
+    LEFT JOIN teams_misc tm on ta.GAME_ID = tm.GAME_ID and ta.TEAM_ID = tm.TEAM_ID
+    LEFT JOIN teams_scoring ts on ta.GAME_ID = ts.GAME_ID and ta.TEAM_ID = ts.TEAM_ID
+    LEFT JOIN lines_table linet on ta.GAME_ID = linet.GAME_ID and ta.TEAM_ABBREVIATION = linet.TEAM_ABBREVIATION
+""").df()
+
+# %%
+### COMBINE TEAM DATA
+conn.execute(f"""
+    CREATE OR REPLACE TABLE teams_combined as
+    SELECT *
+    FROM log_table lt
+    INNER JOIN teams_advanced ta on lt.GAME_ID = ta.GAME_ID and lt.TEAM_ID = ta.TEAM_ID
+    INNER JOIN teams_fourfactors tff on ta.GAME_ID = tff.GAME_ID and ta.TEAM_ID = tff.TEAM_ID
+    INNER JOIN teams_misc tm on ta.GAME_ID = tm.GAME_ID and ta.TEAM_ID = tm.TEAM_ID
+    INNER JOIN teams_scoring ts on ta.GAME_ID = ts.GAME_ID and ta.TEAM_ID = ts.TEAM_ID
+    INNER JOIN lines_table linet on ta.GAME_ID = linet.GAME_ID and ta.TEAM_ABBREVIATION = linet.TEAM_ABBREVIATION
+""").df()
+
+# %%
+### COMBINE PLAYER DATA
+conn.execute(f"""
+    CREATE OR REPLACE TABLE players_combined as
+    SELECT *
+    FROM log_table lt
+    inner join players_advanced ta on ta.GAME_ID = lt.GAME_ID and ta.TEAM_ABBREVIATION = lt.TEAM_ABBREVIATION
+    LEFT JOIN players_fourfactors tff on ta.GAME_ID = tff.GAME_ID and ta.PLAYER_NAME = tff.PLAYER_NAME
+    LEFT JOIN players_misc tm on ta.GAME_ID = tm.GAME_ID and ta.PLAYER_NAME = tm.PLAYER_NAME
+    LEFT JOIN players_scoring ts on ta.GAME_ID = ts.GAME_ID and ta.PLAYER_NAME = ts.PLAYER_NAME
+""").df()
+
+
+
+# %%
+
+conn.execute(f"""
+    SELECT 
+    * 
+             from 
+             players_combined
+             limit 100
+             """).df()
+
+# %%
+conn.execute(f"""
+    SELECT 
+             distinct season_id, count(*)
+
+    FROM players_combined
+             
+    group by season_id
+    order by season_id
+             """).df()
+
+# %%
+conn.execute(f"""
+    SELECT 
+    * 
+             from 
+             players_combined
+             where PLAYER_NAME = 'Damian Lillard'
+             and season_id = 22023
+             limit 100
+             """).df()
+
+
+# %%
+conn.execute(f"""
+    COPY 
+             (SELECT 
+    * 
+             from 
+             players_combined
+             WHERE season_id = 22023
+             order by GAME_DATE
+             )
+    to 'players_combined_2023-24.csv' (HEADER, DELIMITER ',')
+             """).df()
+
+# %%
+
+conn.execute(f"""
+    SELECT 
+             distinct season_id, count(*)
+
+    FROM TEAMS_COMBINED
+             
+    group by season_id
+    order by season_id
+             """).df()
+
+# %%
+
+conn.execute(f"""
+    SELECT 
+             distinct season_id, count(*)
+
+    FROM TEAMS_COMBINED2
+             
+    group by season_id
+    order by season_id
+             """).df()
+
+
+
+# %%
+
+new = conn.execute(f"""
+    SELECT *
+
+    FROM TEAMS_COMBINED2
+             
+    WHERE season_ID = 42022
+    LIMIT 10
+             """).df()
+
+new.to_csv('ex1.csv')
+
+
+# %%
+
+# new = 
+conn.execute(f"""
+    SELECT *
+
+    FROM TEAMS_COMBINED
+             
+    WHERE season_ID = 42019
+    LIMIT 10
+             """).df()
+
+
+new.to_csv('ex1.csv')
+
+
+### explore
+# %%
+
+conn.execute(f"""
+    SELECT count(*)
+
+    FROM players_fourfactors
+             
+    LIMIT 10
+             """).df()
+
+
+
+
+# %%
+
+conn.execute(f"""
+    SELECT count(*)
+
+    FROM players_fourfactors
+             
+    LIMIT 10
+             """).df()
+
+# %%
+conn.execute(f"""
+    SELECT count(*)
+
+    FROM players_combined
+             
+    LIMIT 10
+             """).df()
+
+
+# %%
+
+conn.execute(f"""
+    SELECT count(*)
+
+    FROM teams_fourfactors
+             
+    LIMIT 10
+             """).df()
+
+# %%
+conn.execute(f"""
+    SELECT count(*)
+
+    FROM teams_combined
+             
+    LIMIT 10
+             """).df()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+conn.execute(f"""select *
+             
+             from players_advanced
+             
+             limit 10""").fetchall()
+
+# %%
+conn.execute(f"""select POSS
+             
+             from teams_advanced
+             
+             limit 10""").fetchall()
+# %%
+colset =[]
+for kind in current_tables:
+    sv_files = glob.glob(f'DATA/raw/teams/{kind}/*.csv')
+    newdf = pd.read_csv(sv_files[0])
+    print(len(newdf.columns))
+    colset.append(set(newdf.columns))
+
+
+# %%
+print(colset[0].intersection(colset[1].intersection(colset[2].intersection(colset[3]))))
+
+# %%
+colset =[]
+for kind in current_tables:
+    sv_files = glob.glob(f'DATA/raw/players/{kind}/*.csv')
+    newdf = pd.read_csv(sv_files[0])
+    print(len(newdf.columns))
+    colset.append(set(newdf.columns))
+
+
+# %%
+print(colset[0].intersection(colset[1].intersection(colset[2].intersection(colset[3]))))
+# %%
+columns = conn.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'teams_combined'").fetchall()
+
+
+print(type(columns))
+print(columns)
+# %%
+
+
+conn.execute("SELECT * FROM teams_scoring where GAME_ID = '0021201227'").fetchdf()
+
+
+
+# %%
+# log_csv_files = glob.glob(f'DATA/raw/log/*.csv')
+# for i in log_csv_files:
+#     newdf = pd.read_csv(i)
+#     print(i)
+#     print(len(newdf.columns))
+
+
+# %%
+
+# newdf = pd.read_csv('DATA/raw/log/log_2021-22.csv')
+# # newdf.drop_index()
+# print(newdf.columns)
+# newdf.drop(newdf.columns[[0]], axis=1, inplace = True)
+# print(newdf.columns)
+# newdf.to_csv('DATA/raw/log/log_2021-22.csv', index=False)
+
+
+
